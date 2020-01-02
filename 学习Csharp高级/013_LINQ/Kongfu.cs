9eaedc3086b9d03ef6c50d4c1496f286389ddf04
Erea1 <1207942029@qq.coom>
2020-01-02 22:55:29 +0800

    2020/1/2学习

---
+++ 学习Csharp高级/013_LINQ/Kongfu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_LINQ
+{
+    class Kongfu
+    {
+        public int KongfuId { get; set; }
+        public string KongfuName { get; set; }
+        public int Lethality { get; set; }
+        public override string ToString()
+        {
+            return string.Format("KongfuId:{0},KongfuName:{1},Lethality:{2}",KongfuId,KongfuName,Lethality);
+        }
+    }
+}
