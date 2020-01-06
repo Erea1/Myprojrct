d1024cc76f77ea424c9cfaf20049cd1a95543fbc
Erea1 <1207942029@qq.coom>
2020-01-06 22:34:56 +0800

    2020 1 6 学习

---
+++ 学习Csharp高级/20_线程问题_征用条件/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _20_线程问题_征用条件
+{
+    class Program
+    {
+        static void ChangeState(object o)
+        {
+            MythreadObject m = o as MythreadObject;
+            while (true)
+            {
+                lock (m)//向系统申请可不可以锁定m对象 如果没有 则可以 否则会暂停 直到申请到m对象
+                {
+                    m.ChangeState();//在同一时刻 只有一个线程在执行这个方法
+                }
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            MythreadObject m = new MythreadObject();
+            Thread t = new Thread(ChangeState);
+            t.Start(m);
+
+            new Thread(ChangeState).Start(m);
+            Console.ReadKey();
+        }
+    }
+}
